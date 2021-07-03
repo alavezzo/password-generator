6b929e73858b7cf026c37b117e9d55e3ec169164
bowdenshot <anthonyelavezzo@gmail.com>
2021-07-03 03:22:54 -0500

    created prompt requesting input of desired password length

--- script.js
+++ script.js
@@ -4,8 +4,24 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var getPasswordLength = function() {
+  var length = prompt("Choose password length. Length must be a minimum 8 characters and a maximum of 128 characters");
+  if (length < 8 || length > 128) {
+    alert('Input invalid. Enter a valid length.');
+    getPasswordLength();
+  }
+  else {
+  console.log("Your chosen length is " + length);
+  return length;
+  }
+}
+
+var passwordInfo = {
+}
 // Write password to the #password input
+
 function writePassword() {
+  var length = getPasswordLength();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +31,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
