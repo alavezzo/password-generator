cf480f86c32e531165584c4346e01123a6c25a15
bowdenshot <anthonyelavezzo@gmail.com>
2021-07-03 03:42:29 -0500

    added prompts asking whether or not to include upper and lowercase letter

--- script.js
+++ script.js
@@ -16,12 +16,51 @@ var getPasswordLength = function() {
   }
 }
 
-var passwordInfo = {
+var getUppercase = function() {
+  
+  var uppercase = prompt("Uppercase letters included? Enter 'Yes' or 'No'")
+  uppercase = uppercase.toLowerCase();
+
+  if (uppercase === 'yes') {
+    return true;
+  }
+
+  else if (uppercase === 'no') {
+    return false;
+  }
+
+  else {
+    alert("You need to provide a valid answer! Please try again.");
+    return getUppercase();
+}
+}
+
+var getLowercase = function() {
+  
+  var lowercase = prompt("Lowercase letters included? Enter 'Yes' or 'No'")
+  lowercase = lowercase.toLowerCase();
+
+  if (lowercase === 'yes') {
+    return true;
+  }
+
+  else if (lowercase === 'no') {
+    return false;
+  }
+
+  else {
+    alert("You need to provide a valid answer! Please try again.");
+    return getLowercase();
+}
 }
 // Write password to the #password input
 
 function writePassword() {
   var length = getPasswordLength();
+  var uppercase = getUppercase();
+  console.log(uppercase);
+  var lowercase = getLowercase();
+  console.log(lowercase);
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
